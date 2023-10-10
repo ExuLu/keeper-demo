5416088308c7ae154d7d1b528ab223b4480ce46b
Alena Sharai <exultantislupus@gmail.com>
2023-10-10 11:26:22 +0400

    fix bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-const currentYear = new Date().getFullYear();
-
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <p>Copyright ⓒ {currentYear} </p>
