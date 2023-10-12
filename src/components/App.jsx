4a50761be919c01d430c5ecbe5d99d2f21c743f2
Alena Sharai <exultantislupus@gmail.com>
2023-10-12 14:12:35 +0400

    add function for adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(inputText) {
+    console.log(inputText);
+    setNotes([...notes, inputText]);
+  }
+
   return (
     <div>
       <Header />
+      <CreateArea onClick={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
+      {/* <Note key={1} title='Note title' content='Note content' /> */}
       <Footer />
-      {notes.map((note) => (
-        <Note key={note.key} title={note.title} content={note.content} />
-      ))}
     </div>
   );
 }
