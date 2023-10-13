a6790d31e3b3a5d5a768643959c2c373f56c103c
Alena Sharai <exultantislupus@gmail.com>
2023-10-13 14:20:09 +0400

    fix bugs, add styles

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
-    
   const [inputText, setInputText] = useState({
     title: '',
     content: '',
   });
+  const [titleClicked, setTitleClicked] = useState(false);
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -14,29 +17,39 @@ function CreateArea(props) {
       [name]: value,
     }));
   }
-  function handleClick(e) {
+  function handleButtonClick(e) {
     e.preventDefault();
     props.onClick(inputText);
     setInputText({ ...inputText, title: '', content: '' });
   }
+  function handleTitleClick() {
+    setTitleClicked(true);
+  }
 
   return (
     <div>
-      <form>
+      <form className='create-note'>
         <input
+          onClick={handleTitleClick}
           onChange={handleChange}
           value={inputText.title}
           name='title'
           placeholder='Title'
         />
+
         <textarea
           onChange={handleChange}
+          hidden={!titleClicked && 'true'}
           value={inputText.content}
           name='content'
           placeholder='Take a note...'
-          rows='3'
         />
-        <button onClick={handleClick}>Add</button>
+
+        <Zoom in={titleClicked && true}>
+          <Fab onClick={handleButtonClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props) {
-    
   function handleClick() {
     props.onClick(props.id);
   }
@@ -10,7 +10,9 @@ function Note(props) {
     <div className='note' key={props.key} id={props.id}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
