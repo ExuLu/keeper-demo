b29668abecef372c0b6074e1e80b4030d084a568
Alena Sharai <exultantislupus@gmail.com>
2023-10-16 10:09:14 +0400

    change funcitons and states, delete key prop

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -5,49 +5,56 @@ import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   // Change to two different changes
-  const [inputText, setInputText] = useState({
-    title: '',
-    content: '',
-  });
+  // const [inputText, setInputText] = useState({
+  //   title: '',
+  //   content: '',
+  // });
+
+  const [titleInput, setTitleInput] = useState('');
+  const [contentInput, setContentInput] = useState('');
+
   const [titleClicked, setTitleClicked] = useState(false);
 
   // Add arrow function
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setInputText((prevNote) => ({
-      ...prevNote,
-      [name]: value,
-    }));
-  }
+  // function handleChange(e) {
+  //   const { name, value } = e.target;
+  //   setInputText((prevNote) => ({
+  //     ...prevNote,
+  //     [name]: value,
+  //   }));
+  // }
   function handleButtonClick(e) {
     e.preventDefault();
-    props.onClick(inputText);
-    setInputText({ ...inputText, title: '', content: '' });
+    props.onClick({ title: titleInput, content: contentInput });
+    setTitleInput('');
+    setContentInput('');
   }
-  function handleTitleClick() {
-    setTitleClicked(true);
-  }
+  // function handleTitleClick() {
+  //   setTitleClicked(true);
+  // }
 
   return (
     <div>
       <form className='create-note'>
         <input
-          onClick={handleTitleClick}
+          onClick={() => setTitleClicked(true)}
           // Add arrow function
-          onChange={handleChange} 
-          value={inputText.title}
+          onChange={(e) => setTitleInput(e.target.value)}
+          value={titleInput}
           name='title'
           placeholder='Title'
         />
 
-        <textarea
-          // Add arrow function
-          onChange={handleChange}
-          hidden={!titleClicked && 'true'}
-          value={inputText.content}
-          name='content'
-          placeholder='Take a note...'
-        />
+        {titleClicked && (
+          <textarea
+            // Add arrow function
+            onChange={(e) => setContentInput(e.target.value)}
+            // hidden={!titleClicked && 'true'}
+            value={contentInput}
+            name='content'
+            placeholder='Take a note...'
+          />
+        )}
 
         <Zoom in={titleClicked && true}>
           <Fab onClick={handleButtonClick}>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,7 +7,7 @@ function Note(props) {
   }
 
   return (
-    <div className='note' key={props.key} id={props.id}>
+    <div className='note' id={props.id}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button onClick={handleClick}>
