1640e645559b534ca4ec87475fcbd90d2b782bb5
Alena Sharai <exultantislupus@gmail.com>
2023-10-16 10:10:50 +0400

    delete comments

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -4,41 +4,23 @@ import AddIcon from '@mui/icons-material/Add';
 import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
-  // Change to two different changes
-  // const [inputText, setInputText] = useState({
-  //   title: '',
-  //   content: '',
-  // });
-
   const [titleInput, setTitleInput] = useState('');
   const [contentInput, setContentInput] = useState('');
 
   const [titleClicked, setTitleClicked] = useState(false);
 
-  // Add arrow function
-  // function handleChange(e) {
-  //   const { name, value } = e.target;
-  //   setInputText((prevNote) => ({
-  //     ...prevNote,
-  //     [name]: value,
-  //   }));
-  // }
   function handleButtonClick(e) {
     e.preventDefault();
     props.onClick({ title: titleInput, content: contentInput });
     setTitleInput('');
     setContentInput('');
   }
-  // function handleTitleClick() {
-  //   setTitleClicked(true);
-  // }
 
   return (
     <div>
       <form className='create-note'>
         <input
           onClick={() => setTitleClicked(true)}
-          // Add arrow function
           onChange={(e) => setTitleInput(e.target.value)}
           value={titleInput}
           name='title'
@@ -47,9 +29,7 @@ function CreateArea(props) {
 
         {titleClicked && (
           <textarea
-            // Add arrow function
             onChange={(e) => setContentInput(e.target.value)}
-            // hidden={!titleClicked && 'true'}
             value={contentInput}
             name='content'
             placeholder='Take a note...'
