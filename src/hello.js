1a3b234d3a04b6d378302c088025aa09e44559ad
Alena Sharai <exultantislupus@gmail.com>
2023-10-30 12:57:45 +0400

    add .ts file

---
+++ src/hello.js
@@ -0,0 +1,4 @@
+function greet(person, date) {
+    console.log("Hello, ".concat(person, ", today is ").concat(date));
+}
+greet('Alena', '30th of October');
---
+++ src/hello.ts
@@ -0,0 +1,5 @@
+function greet(person, date) {
+  console.log(`Hello, ${person}, today is ${date}`);
+}
+
+greet('Alena', '30th of October');
